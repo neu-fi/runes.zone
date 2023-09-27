9686b20564a1f3281a347475ded1e5c7816da2ca
Han Tuzun <hantuzun@users.noreply.github.com>
2023-09-27 18:31:04 +0300

    Log issued tickers

--- app/api/cron/persist-runes/route.ts
+++ app/api/cron/persist-runes/route.ts
@@ -1,12 +1,117 @@
 import { RPCClient } from "rpc-bitcoin"
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import * as varuint from 'varuint-bitcoin'
+var Buffer = require('safe-buffer').Buffer
+
+let RUNE_STARTING_ASM = 'OP_RETURN 82'
+let RUNE_FIRST_BLOCK = 809385
 
 const url = process.env.BITCOIN_RPC_URL  || ""
 const port = Number.parseInt(process.env.BITCOIN_RPC_PORT || "0")
 const user = process.env.BITCOIN_RPC_USER  || ""
 const pass = process.env.BITCOIN_RPC_PASS  || ""
 const client = new RPCClient({ url, port, user, pass, timeout: 10000 })
+
+// str must be uppercase A-Z only!
+function encodeBijectiveBase26(str: string) {
+  let encode = 0
+  if ( /^([A-Z])+$/.test(str) ) {
+    str.split('').forEach( (c, i) => {
+      let charIndex = c.charCodeAt(0) - "A".charCodeAt(0) + 1
+      let exponent = str.length - 1 - i
+      let multiplier = 26 ** exponent
+      let letterEncode = charIndex * multiplier
+      encode += letterEncode
+    })
+  }
+
+  return encode
+}
+
+function decodeBijectiveBase26(num: number) {
+  if ( num < 1 ) {
+    return ''
+  } else {
+    let letters = []
+    while ( 0 < num ) {
+      let remainder = num % 26
+      letters.unshift( remainder )
+      num = (num - remainder) / 26
+    }
+    return letters.map(letterIndex => String.fromCharCode(65 + letterIndex)).join('')
+  }
+}
+
+export function decodeVaruintSequence(hexadecimalVaruintSequence: string) {
+  let buffer = Buffer.from(hexadecimalVaruintSequence, 'hex')
+  
+  let offset = 0
+  let decodedIntegers = []
+  while ( offset < buffer.length ) {
+    let decoded: number
+    try {
+      decoded = varuint.decode(buffer, offset)
+      decodedIntegers.push(decoded)
+    } catch (e) {
+      console.error(e)
+      // Bandaid as the library we use cannot handle large integers that can be represented in varuint
+      decodedIntegers.push(-1)
+    }
+
+    let firstByte = buffer[offset]
+    if ( firstByte === 0xff ) {
+      offset += 9
+    } else if ( firstByte === 0xfe ) {
+      offset += 5
+    } else if ( firstByte === 0xfd ) {
+      offset += 3
+    } else {
+      offset += 1
+    }
+  }
+  return decodedIntegers
+}
+
+export function decodeTransfer(hexadecimalVaruintSequence: string) {
+  return decodeVaruintSequence(hexadecimalVaruintSequence)
+}
+
+export function decodeIssuance(hexadecimalVaruintSequence: string) {
+  return decodeVaruintSequence(hexadecimalVaruintSequence)
+}
+
+export function logRuneIssuance(tx: any) {
+  for (let out of tx['vout']) {
+    let type: string = out['scriptPubKey']['type']
+    let asm: string = out['scriptPubKey']['asm']
+    if ( type === 'nulldata' && asm.startsWith(RUNE_STARTING_ASM) ) {
+      let runeMessages = asm.slice(RUNE_STARTING_ASM.length).split(' ').filter(s => s !== '')
+      // console.log(tx['hash'], 'has', asm, 'with', runeMessages.length, 'rune messages:')
+      
+      if ( runeMessages.length === 2 ) {
+        let transferMessage = runeMessages[0]
+        let decodedTransferMessage = decodeVaruintSequence(transferMessage)
+        let issuanceMessage = runeMessages[1]
+        let decodedIssuanceMessage = decodeVaruintSequence(issuanceMessage)
+        if ( decodedTransferMessage.length % 3 !== 0) {
+          // console.error('transfer_invalid_length')
+          return
+        }
+        if ( decodedIssuanceMessage.length !== 2) {
+          // console.error('issuance_invalid_length')
+          return
+        }
+
+        // console.log('txhash:', tx['hash'])
+        // console.log('transfer:', decodedTransferMessage)
+        // console.log('issuance:', decodedIssuanceMessage)
+        console.log('Issued', decodeBijectiveBase26(decodedIssuanceMessage[0]), 'with', decodedIssuanceMessage[1], 'digits')
+        // console.log('')
+      }
+    }
+  }
+}
 
 export async function GET(request: NextRequest) {
   let rawCount = request.nextUrl.searchParams.get('count')
@@ -51,22 +156,16 @@ export async function GET(request: NextRequest) {
 
   let blockheights = [...Array(count).keys()].map(index => index + (blockcount - count + 1))
   for (let blockheight of blockheights) {
-    console.log('')
-    console.log("---=== block(",blockheight,")>> ===---")
-    console.log('')
+    // console.log('')
+    // console.log("---=== block(",blockheight,")>> ===---")
+    // console.log('')
     const blockhash = await client.getblockhash({ height: blockheight })
     const block = await client.getblock({ blockhash, verbosity: 2 })
     for (let tx of block['tx']) {
-      for (let out of tx['vout']) {
-        if ( out['scriptPubKey']['type'] === 'nulldata' && out['scriptPubKey']['asm'].startsWith('OP_RETURN 82') ) {
-          console.log(tx['hash'], '->')
-          console.log(out['scriptPubKey']['asm'])
-          console.log('')
-        }
-      }
+      logRuneIssuance(tx)
     }
-    console.log("---=== <<block(",blockheight,") ===---")
-    console.log('')
+    // console.log("---=== <<block(",blockheight,") ===---")
+    // console.log('')
   }
 
   return NextResponse.json(
@@ -78,14 +177,3 @@ export async function GET(request: NextRequest) {
     }
   )
 }
-
-// list: address / runes as badges (ids first, names later) / issue|send badges / View button->modal
-// badges: icons
-// issue:
-// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-//   <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
-// </svg>
-// paper:
-// <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-//   <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-// </svg>
